ecdaf21df2c0146e43b9550f2277c6d7dc5a3f4a
RayyanNafees <nafees.rayyan@gmail.com>
2022-01-26 22:10:37 +0530

    Home & NFT done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,8 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-
+import Hidden from "@mui/material/Hidden";
+import MenuIcon from "@mui/icons-material/Menu";
 function Home() {
   return (
     <Grid>
@@ -21,81 +22,108 @@ function Home() {
           width: "100vw",
         }}
       >
-        <Grid
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          fullWidth
-          direction="row"
-          mb={25}
-        >
-          <Grid item container spacing={3} sm={6} my={0.1}>
-            <Grid item>
-              <img src={logo} style={{ width: 50, height: 50 }} />
+        <Hidden smDown>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="center"
+            fullWidth
+            direction="row"
+            mb={25}
+          >
+            <Grid item container spacing={3} sm={6} my={0.1}>
+              <Grid item>
+                <img src={logo} style={{ width: 50, height: 50 }} />
+              </Grid>
+              <Grid item>
+                <IconButton style={{ color: "white" }}>
+                  <InstagramIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton style={{ color: "white" }}>
+                  <TwitterIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton style={{ color: "white" }}>
+                  <YouTubeIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton style={{ color: "white" }}>
+                  <TelegramIcon fontSize="large" />
+                </IconButton>
+              </Grid>
             </Grid>
-            <Grid item>
-              <IconButton style={{ color: "white" }}>
-                <InstagramIcon fontSize="large" />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton style={{ color: "white" }}>
-                <TwitterIcon fontSize="large" />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton style={{ color: "white" }}>
-                <YouTubeIcon fontSize="large" />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton style={{ color: "white" }}>
-                <TelegramIcon fontSize="large" />
-              </IconButton>
+            <Grid item container sm={6} spacing={3} my={0.1}>
+              <Grid item>
+                <Button>Home</Button>
+              </Grid>
+              <Grid item>
+                <Button>Whitepaper</Button>
+              </Grid>
+              <Grid item>
+                <Button>NFT</Button>
+              </Grid>
+              <Grid item>
+                <Button>Marketplace</Button>
+              </Grid>
+              <Grid item>
+                <Button>Community</Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  bgColor="orange"
+                  style={{ textTransform: "none" }}
+                >
+                  Connect Wallet
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid item container sm={6} spacing={3} my={0.1}>
-            <Grid item>
-              <Button>Home</Button>
-            </Grid>
-            <Grid item>
-              <Button>Whitepaper</Button>
-            </Grid>
-            <Grid item>
-              <Button>NFT</Button>
-            </Grid>
-            <Grid item>
-              <Button>Marketplace</Button>
-            </Grid>
-            <Grid item>
-              <Button>Community</Button>
-            </Grid>
-            <Grid item>
-              <Button
-                variant="contained"
-                bgColor="orange"
-                style={{ textTransform: "none" }}
+        </Hidden>
+        <Hidden mdUp>
+          <Grid container fullWidth justifyContent={"flex-end"} px={2} py={2}>
+            <IconButton color="warning">
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+        <Hidden smDown>
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid alignItems="center" justifyContent="center">
+              <img src={logo} style={{ height: "40vh", width: "40vw" }} />
+              <Typography
+                variant="h2"
+                color="initial"
+                color="white"
+                align="center"
+                fontWeight={700}
               >
-                Connect Wallet
-              </Button>
+                Battle Blast
+              </Typography>
             </Grid>
           </Grid>
-        </Grid>
+        </Hidden>
+        <Hidden mdUp>
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid alignItems="center" justifyContent="center" mt={40}>
+            <img src={logo} style={{ height: "20vh", width: "100%" }} />
+              <Typography
+                variant="h3"
+                color="initial"
+                color="white"
+                align="center"
+                fontWeight={700}
 
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid alignItems="center" justifyContent="center">
-            <img src={logo} style={{ height: "40vh", width: "40vw" }} />
-            <Typography
-              variant="h2"
-              color="initial"
-              color="white"
-              align="center"
-              fontWeight={700}
-            >
-              Battle Blast
-            </Typography>
+              >
+                Battle Blast
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
+        </Hidden>
       </Grid>
     </Grid>
   );
--- src/pages/NFT.js
+++ src/pages/NFT.js
@@ -1,30 +1,56 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import nft from "../assets/nft.svg";
+import nftMobile from "../assets/nft.mobile.svg";
 import Typography from "@mui/material/Typography";
+import Hidden from "@mui/material/Hidden";
 function NFT() {
   return (
     <Grid>
-      <Grid
-        style={{
-          background: `url(${nft})`,
-          backgroundSize: "100%",
-          height: "100vh",
-          backgroundRepeat: "no-repeat",
-          backgroundPositionY: "bottom",
-        }}
-        pt={70}
-      >
-        <Typography
-          variant="h2"
-          color="initial"
-          color="white"
-          align="center"
-          fontWeight={700}
+      <Hidden smDown>
+        <Grid
+          style={{
+            background: `url(${nft})`,
+            backgroundSize: "100%",
+            height: "100vh",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionY: "bottom",
+          }}
+          pt={70}
         >
-          Coming Soon
-        </Typography>
-      </Grid>
+          <Typography
+            variant="h2"
+            color="initial"
+            color="white"
+            align="center"
+            fontWeight={700}
+          >
+            Coming Soon
+          </Typography>
+        </Grid>
+      </Hidden>
+      <Hidden mdUp>
+        <Grid
+          style={{
+            background: `url(${nftMobile})`,
+            backgroundSize: "100%",
+            height: "30vh",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionY: "bottom",
+          }}
+          pt={20}
+        >
+          <Typography
+            variant="h4"
+            color="initial"
+            color="white"
+            align="center"
+            fontWeight={500}
+          >
+            Coming Soon
+          </Typography>
+        </Grid>
+      </Hidden>
     </Grid>
   );
 }
