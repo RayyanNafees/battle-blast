720fbb471432ab29574c095b94407a3f32957a82
RayyanNafees <nafees.rayyan@gmail.com>
2022-01-26 11:34:57 +0530

    Init

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@mui/material/Grid'
+import home from '../assets/home.svg'
+
+
+function Home() {
+  return <Grid>
+    <Grid>
+
+    </Grid>
+  </Grid>;
+}
+
+export default Home;
