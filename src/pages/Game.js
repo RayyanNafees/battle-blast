c20d6798dc89e70934aa74d90957dbb4d3bb5357
RayyanNafees <nafees.rayyan@gmail.com>
2022-01-26 22:14:09 +0530

    Game.js done

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,29 +2,54 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import game from "../assets/game.svg";
 import Typography from "@mui/material/Typography";
+import Hidden from "@mui/material/Hidden";
 function Game() {
   return (
     <Grid>
-      <Grid
-        style={{
-          background: `url(${game})`,
-          backgroundSize: "100%",
-          height: "100vh",
-          backgroundRepeat: "no-repeat",
-          backgroundPositionY: "bottom",
-        }}
-        pt={70}
-      >
-        <Typography
-          variant="h2"
-          color="initial"
-          color="white"
-          align="center"
-          fontWeight={700}
+      <Hidden smDown>
+        <Grid
+          style={{
+            background: `url(${game})`,
+            backgroundSize: "100%",
+            height: "100vh",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionY: "bottom",
+          }}
+          pt={70}
         >
-          Coming Soon
-        </Typography>
-      </Grid>
+          <Typography
+            variant="h2"
+            color="initial"
+            color="white"
+            align="center"
+            fontWeight={700}
+          >
+            Coming Soon
+          </Typography>
+        </Grid>
+      </Hidden>
+      <Hidden mdUp>
+        <Grid
+          style={{
+            background: `url(${game})`,
+            backgroundSize: "100%",
+            height: "25vh",
+            backgroundRepeat: "no-repeat",
+            backgroundPositionY: "bottom",
+          }}
+          pt={15}
+        >
+          <Typography
+            variant="h4"
+            color="initial"
+            color="white"
+            align="center"
+            fontWeight={700}
+          >
+            Coming Soon
+          </Typography>
+        </Grid>
+      </Hidden>
     </Grid>
   );
 }
